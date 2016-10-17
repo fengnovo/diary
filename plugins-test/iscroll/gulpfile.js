463f6edcda85affc8ef7c46c6416bc5951d94581
fengnovo <fengnoku@gmail.com>
2016-10-17 09:43:21 +0800

    10-17-iscroll-test
    10-17-iscroll-test, plugins test

---
+++ plugins-test/iscroll/gulpfile.js
@@ -0,0 +1,90 @@
+var gulp = require('gulp'),
+    path = require('path'),
+    del = require('del'),
+    browserSync = require('browser-sync').create(),
+    historyApiFallback = require('connect-history-api-fallback'),
+    minimist = require('minimist'),
+    runSequence = require('run-sequence'),
+    uglify = require('gulp-uglify'),        //js压缩
+    concat = require('gulp-concat'),        //文件合并
+    minifycss = require('gulp-minify-css'); //css压缩
+
+
+var argv = minimist(process.argv.slice(2), { boolean: true });
+console.dir(argv);
+
+
+var SRC_DIR = './src/';
+var DIST_DIR = './web/';
+
+var AUTO_PREFIXER_RULES = [
+    'ie >= 9',
+    'chrome >= 34',
+    'safari >= 6',
+    'ios >= 7',
+    'android >= 4.1'
+];
+
+
+
+gulp.task('browser-sync', function () {
+    var proxyMiddleware = require('http-proxy-middleware');
+    var proxy1 = proxyMiddleware('/list', {target: 'http://10.2.130.20:9000/'});
+    var proxy2 = proxyMiddleware('/action', {target: 'http://10.2.130.20:9000/'});
+    var proxy3 = proxyMiddleware('/read', {target: 'http://10.2.130.20:9000/'});
+    browserSync.init({
+        server: {
+            baseDir: DIST_DIR,
+            middleware: [historyApiFallback(), proxy1,proxy2,proxy3]
+        }
+    });
+});
+
+gulp.task('copy_iscroll', function(cb) {       //开发复制资讯直播列表部分
+    return gulp.src('./src/*')
+        .pipe(gulp.dest('./web/'));
+});
+
+
+
+gulp.task('copy_build_iscroll_js', function() {
+    return gulp.src(['./src/zepto.min.js','./src/iscroll.js','./src/main.js'])
+        .pipe(concat('main.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('./web/'));
+});
+
+gulp.task('copy_build_iscroll_css', function() {
+    return gulp.src('./src/*.css')
+        .pipe(minifycss())
+        .pipe(gulp.dest('./web/')) ;
+});
+
+gulp.task('copy_build_iscroll', function() {
+    return gulp.src(['./src/*.gif','./src/*.png','./src/*.html'])
+        .pipe(gulp.dest('./web/')) ;
+});
+
+gulp.task('build_iscroll', function() {      
+    gulp.start(['copy_build_iscroll_js', 'copy_build_iscroll_css','copy_build_iscroll']);
+});
+
+
+
+
+
+/* build && clean */
+gulp.task('clean', function(cb) {
+    return del(['./web/'], { force: true }, cb);
+});
+
+
+gulp.task('default', function (cb) {
+    runSequence('clean','browser-sync', 'copy_iscroll', function () {
+        cb && cb();
+    })
+});
+
+gulp.task('build', ['clean'], function(){
+    gulp.start(['build_liveList']);
+});
---
+++ plugins-test/iscroll/src/main.js
@@ -0,0 +1,178 @@
+$(function(){
+	var myScroll,
+		pullDownEl, pullDownOffset,
+		pullUpEl, pullUpOffset;
+	var page = 1, num = 10, canLoad = false, canPullDown = false;
+
+	var line = function(){
+		$('#line').height($('#thelist').height());
+	}
+	function returnHtml(list){
+		Date.prototype.Format = function (fmt) { //author: meizz
+			var o = {
+				"M+": this.getMonth() + 1, //月份
+				"d+": this.getDate(), //日
+				"h+": this.getHours(), //小时
+				"m+": this.getMinutes(), //分
+				"s+": this.getSeconds(), //秒
+				"q+": Math.floor((this.getMonth() + 3) / 3), //季度
+				"S": this.getMilliseconds() //毫秒
+			};
+			if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+			for (var k in o)
+				if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+			return fmt;
+		}
+		var _html = '';
+		list.map(function(item){
+			return  _html+= '<li id='+item.id+'><p>'+new Date(item.publishTime* 1000).Format("MM月dd日")+'<span>'+new Date(item.publishTime* 1000).Format("hh:mm")+'</span></p><b></b><dl><dt>'+item.content+'</dt></dl></li>';
+		});
+		return _html;
+	}
+
+	var loadData = function(){
+		url = 'http://10.2.130.20:9000/list/live/column/57e1f9204cb4c19258cf1f7d?page=1&pageSize=10';
+		$.ajax({        //资讯详情接口
+            url: url,
+            type: 'GET',
+            dataType: "json",
+            timeout: 10000,
+            error: function(){},
+            success: function (data) {
+            	page++;
+				$('#loadingTip').remove();
+				$('#wrapper').show();
+				var list = data.data;
+				$('#listCon').html(returnHtml(list));
+				line();
+				loaded();
+				myScroll.refresh();
+				canLoad = true;
+				canPullDown = true;
+			}
+        });
+	}
+
+	loadData();
+
+
+	/**
+	 * 下拉刷新 （自定义实现此方法）
+	 * myScroll.refresh();		// 数据加载完成后，调用界面更新方法
+	 */
+	function pullDownAction () {
+		if(canPullDown) {
+			canPullDown = false;//57e1f9204cb4c19258cf1f7d
+			url = 'http://10.2.130.20:9000/list/live/column/57e1f9204cb4c19258cf1f7d?page=1&pageSize=10';
+			$.ajax({        //资讯详情接口
+	            url: url,
+	            type: 'GET',
+	            dataType: "json",
+	            timeout: 10000,
+	            error: function(){},
+	            success: function (data) {
+					page = 2;
+					var list = data.data;
+					$('#listCon').html(returnHtml(list));
+					line();
+					$('#pullUp').show();
+					$('#listEnd').hide();
+					myScroll.refresh();
+					canPullDown = true;
+					canLoad = true;
+				}
+	        });
+
+		}
+	}
+	/**
+	 * 滚动翻页 （自定义实现此方法）
+	 * myScroll.refresh();		// 数据加载完成后，调用界面更新方法
+	 */
+	function pullUpAction () {
+		if(canLoad){
+			canLoad = false;
+			url = 'http://10.2.130.20:9000/list/live/column/57e1f9204cb4c19258cf1f7d?page='+page+'&pageSize=10';
+			$.ajax({        //资讯详情接口
+	            url: url,
+	            type: 'GET',
+	            dataType: "json",  
+	            timeout: 10000,
+	            error: function(){},
+	            success: function (data) {
+					page++;
+					var list = data.data;
+					if(list && list.length === 0){
+						$('#pullUp').hide();
+						$('#line').height($('#thelist').height()+48);
+						$('#listEnd').show();
+						canLoad = false;
+						return;
+					}
+					$('#listCon').append(returnHtml(list));
+					line();
+					myScroll.refresh();
+					canLoad = true;
+				}
+	        });
+		}
+	}
+
+	/**
+	 * 初始化iScroll控件
+	 */
+	function loaded() {
+		pullDownEl = document.getElementById('pullDown');
+		pullDownOffset = pullDownEl.offsetHeight;
+		pullUpEl = document.getElementById('pullUp');
+		pullUpOffset = pullUpEl.offsetHeight;
+
+		myScroll = new iScroll('wrapper', {
+			scrollbarClass: 'myScrollbar', /* 重要样式 */
+			useTransition: false,
+			topOffset: pullDownOffset,
+			onRefresh: function () {
+				if (pullDownEl.className.match('loading')) {
+					pullDownEl.className = '';
+					pullDownEl.querySelector('.pullDownLabel').innerHTML = '下拉刷新...';
+				} else if (pullUpEl.className.match('loading')) {
+					pullUpEl.className = '';
+					pullUpEl.querySelector('.pullUpLabel').innerHTML = '上拉加载更多...';
+				}
+			},
+			onScrollMove: function () {
+				if (this.y > 5 && !pullDownEl.className.match('flip')) {
+					pullDownEl.className = 'flip';
+					pullDownEl.querySelector('.pullDownLabel').innerHTML = '松手开始更新...';
+					this.minScrollY = 0;
+				} else if (this.y < 5 && pullDownEl.className.match('flip')) {
+					pullDownEl.className = '';
+					pullDownEl.querySelector('.pullDownLabel').innerHTML = '下拉刷新...';
+					this.minScrollY = -pullDownOffset;
+				} else if (this.y < (this.maxScrollY - 5) && !pullUpEl.className.match('flip')) {
+					pullUpEl.className = 'flip';
+					pullUpEl.querySelector('.pullUpLabel').innerHTML = '松手开始更新...';
+					this.maxScrollY = this.maxScrollY;
+				} else if (this.y > (this.maxScrollY + 5) && pullUpEl.className.match('flip')) {
+					pullUpEl.className = '';
+					pullUpEl.querySelector('.pullUpLabel').innerHTML = '上拉加载更多...';
+					this.maxScrollY = pullUpOffset;
+				}
+			},
+			onScrollEnd: function () {
+				if (pullDownEl.className.match('flip')) {
+					pullDownEl.className = 'loading';
+					pullDownEl.querySelector('.pullDownLabel').innerHTML = '刷新中...';
+					pullDownAction();	// Execute custom function (ajax call?)
+				} else if (pullUpEl.className.match('flip')) {
+					pullUpEl.className = 'loading';
+					pullUpEl.querySelector('.pullUpLabel').innerHTML = '加载中...';
+					pullUpAction();	// Execute custom function (ajax call?)
+				}
+			}
+		});
+
+		setTimeout(function () { document.getElementById('wrapper').style.left = '0'; }, 800);
+	}
+
+});
