ddadd7ece416f8487dd277ee6369f5a228c691c7
fengnovo <fengnoku@gmail.com>
2016-10-24 23:28:21 +0800

    angualrjq

--- others/angular/20161023/gulp-angular-test/src/js/controllers/a.js
+++ others/angular/20161023/gulp-angular-test/src/js/controllers/a.js
@@ -1,7 +1,37 @@
-!(function() {
-	'use strict';
-	App.a = function($scope, $http, $routeParams) {
-		var xx = 'a页面';
-	};
-	App.a.$inject = ['$scope', '$http', '$routeParams'];
-})(window.App || (window.App = {}));
+$(function(){
+
+        
+    app.controller('a', function ($scope, $route, $) { 
+            $scope.$route = $route;
+            $('#tip').click(function(){
+                alert('a页面');
+            });
+        })
+        .controller('b', function ($scope, $route) { 
+            $scope.$route = $route;
+        })
+        .controller('c', function ($scope, $route) { 
+            $scope.$route = $route;
+        });
+
+        
+});
+var app = angular.module('ngRouteExample', ['ngRoute']);
+app.config(function ($routeProvider) {
+            $routeProvider.
+            when('/a', {
+                templateUrl: 'page/a.html',
+                controller: 'a'
+            }).
+            when('/b', {
+                templateUrl: 'page/b.html',
+                controller: 'b'
+            }).
+            when('/c', {
+                templateUrl: 'page/c.html',
+                controller: 'c'
+            }).
+            otherwise({
+                redirectTo: '/a'
+            });
+        });
--- others/angular/20161023/gulp-angular-test/src/js/controllers/b.js
+++
@@ -1,7 +0,0 @@
-!(function() {
-	'use strict';
-	App.b = function($scope, $http, $routeParams) {
-		var xx = 'b页面';
-	};
-	App.b.$inject = ['$scope', '$http', '$routeParams'];
-})(window.App || (window.App = {}));
