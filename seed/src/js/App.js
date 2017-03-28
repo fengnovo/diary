fbfff8498b1ed9d26d8114a7170d5dc3e4f122c7
huangyunfeng <283563375@qq.com>
2017-03-28 22:59:22 +0800

    save a seed project

---
+++ seed/src/js/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../css/App.css';
+
+class App extends React.Component {
+    constructor (...args) {
+        super(...args);
+    }
+
+    render() {
+        return <div className="font-c">Hello World !</div>;
+    }
+}
+
+export default App;
