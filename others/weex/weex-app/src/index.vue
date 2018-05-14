61e64ee8e43d37fe951a5d79d5056ca591399967
huangyunfeng <huangyunfeng@jd.com>
2018-05-14 15:33:54 +0800

    test

---
+++ others/weex/weex-app/src/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="wrapper">
+    <image :src="logo" class="logo" />
+    <text class="greeting">The environment is ready!</text>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data () {
+    return {
+      logo: 'https://gw.alicdn.com/tfs/TB1yopEdgoQMeJjy1XaXXcSsFXa-640-302.png'
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .wrapper {
+    justify-content: center;
+    align-items: center;
+  }
+  .logo {
+    width: 424px;
+    height: 200px;
+  }
+  .greeting {
+    text-align: center;
+    margin-top: 70px;
+    font-size: 50px;
+    color: #41B883;
+  }
+  .message {
+    margin: 30px;
+    font-size: 32px;
+    color: #727272;
+  }
+</style>
